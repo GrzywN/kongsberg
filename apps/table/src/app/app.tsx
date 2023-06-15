6a0b2a730e7f29fca34d37b037014f15f3bd7bdc
Karol Binkowski <starystachu9@gmail.com>
2023-06-15 14:35:51 +0200

    perf(table): add lazy loading to table routes

--- apps/table/src/app/app.tsx
+++ apps/table/src/app/app.tsx
@@ -1,13 +1,17 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Hero } from './features/hero/components/hero/hero';
-import { StepComments } from './features/table/components/step-comments/step-comments';
-import { StepPosts } from './features/table/components/step-posts/step-posts';
-import { StepUsers } from './features/table/components/step-users/step-users';
 import { Table } from './features/table/components/table/table';
 import { DefaultLayout } from './layouts/default-layout/default-layout';
 import { ReactQueryProvider } from './react-query';
 import { routes } from './shared/utils/routes';
+
+const StepUsers = lazy(() => import('./features/table/components/step-users/step-users'));
+const StepPosts = lazy(() => import('./features/table/components/step-posts/step-posts'));
+const StepComments = lazy(() => import('./features/table/components/step-comments/step-comments'));
+
+// TODO: add table step skeleton / loading ui state
 
 export function App() {
   return (
@@ -18,11 +22,11 @@ export function App() {
           <Routes>
             <Route path={routes.home.path} element={<h1>And this is the home page! Go to /table/users</h1>} />
             <Route path="table" element={<Table />}>
-              <Route path={routes.table.users.path} element={<StepUsers />} />
-              <Route path={routes.table.posts.path} element={<StepPosts />} />
+              <Route path={routes.table.users.path} element={<Suspense fallback={<h1>Is loading...</h1>}><StepUsers /></Suspense>} />
+              <Route path={routes.table.posts.path} element={<Suspense fallback={<h1>Is loading...</h1>}><StepPosts /></Suspense>} />
               <Route
                 path={routes.table.comments.path}
-                element={<StepComments />}
+                element={<Suspense fallback={<h1>Is loading...</h1>}><StepComments /></Suspense>}
               />
               <Route index element={<caption>And this is an empty table. Go to /table/users</caption>} />
             </Route>
