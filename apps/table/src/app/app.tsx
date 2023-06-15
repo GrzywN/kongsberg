86b9c75fde0b720f0d364d61ec8698ee0bf789cb
Karol Binkowski <starystachu9@gmail.com>
2023-06-15 13:41:58 +0200

    feat(table): move routing to the app level

--- apps/table/src/app/app.tsx
+++ apps/table/src/app/app.tsx
@@ -1,9 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Hero } from './features/hero/components/hero/hero';
+import { StepComments } from './features/table/components/step-comments/step-comments';
+import { StepPosts } from './features/table/components/step-posts/step-posts';
+import { StepUsers } from './features/table/components/step-users/step-users';
 import { Table } from './features/table/components/table/table';
 import { DefaultLayout } from './layouts/default-layout/default-layout';
 import { ReactQueryProvider } from './react-query';
+import { routes } from './shared/utils/routes';
 
 export function App() {
   return (
@@ -11,7 +15,18 @@ export function App() {
       <BrowserRouter>
         <DefaultLayout>
           <Hero />
-          <Table />
+          <Routes>
+            <Route path={routes.home.path} element={<h1>And this is the home page! Go to /table/users</h1>} />
+            <Route path="table" element={<Table />}>
+              <Route path={routes.table.users.path} element={<StepUsers />} />
+              <Route path={routes.table.posts.path} element={<StepPosts />} />
+              <Route
+                path={routes.table.comments.path}
+                element={<StepComments />}
+              />
+              <Route index element={<caption>And this is an empty table. Go to /table/users</caption>} />
+            </Route>
+          </Routes>
         </DefaultLayout>
       </BrowserRouter>
     </ReactQueryProvider>
--- apps/table/src/app/features/table/components/table/table.tsx
+++ apps/table/src/app/features/table/components/table/table.tsx
@@ -1,21 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-import { routes } from '../../../../shared/utils/routes';
-import { StepComments } from '../step-comments/step-comments';
-import { StepPosts } from '../step-posts/step-posts';
-import { StepUsers } from '../step-users/step-users';
+export interface TableProps {
+  children?: React.ReactNode;
+}
 
-export function Table() {
-  // TODO: dodać posts i comments do fetchowania wszystkich postów i komentarzy
+export function Table(props: TableProps) {
+  const { children = "" } = props;
 
   return (
     <div className="bg-white relative overflow-x-auto shadow-md sm:rounded-lg h-[37.125rem]">
       <table className="w-full text-sm text-left text-neutral-700 font-semibold">
-        <Routes>
-          <Route path={routes.table.users.path} element={<StepUsers />} />
-          <Route path={routes.table.posts.path} element={<StepPosts />} />
-          <Route path={routes.table.comments.path} element={<StepComments />} />
-        </Routes>
+        {children}
+        <Outlet />
       </table>
     </div>
   );
