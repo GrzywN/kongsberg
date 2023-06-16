1987797dd1fd19663465412a2a7e1a5f944b9652
Karol Binkowski <starystachu9@gmail.com>
2023-06-16 18:48:44 +0200

    test/fix(table): add msw to App spec

--- apps/table/src/app/app.spec.tsx
+++ apps/table/src/app/app.spec.tsx
@@ -1,8 +1,28 @@
+import {
+  closeConnectionToCommentsServer,
+  closeConnectionToPostsServer,
+  closeConnectionToUsersServer,
+  listenToCommentsServer,
+  listenToPostsServer,
+  listenToUsersServer,
+} from '@kongsberg/shared/testing';
 import { render } from '@testing-library/react';
 
 import App from './app';
 
 describe('App', () => {
+  beforeAll(() => {
+    listenToUsersServer();
+    listenToPostsServer();
+    listenToCommentsServer();
+  });
+
+  afterAll(() => {
+    closeConnectionToUsersServer();
+    closeConnectionToPostsServer();
+    closeConnectionToCommentsServer();
+  });
+
   it('should render successfully', () => {
     const { baseElement } = render(<App />);
     expect(baseElement).toBeTruthy();
